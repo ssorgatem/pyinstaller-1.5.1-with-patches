770daa1e574e0082c13bcfd16ec99755eca2f9d4
Adrià Cereto-Massagué <ssorgatem@gmail.com>
2011-12-21 22:27:41 +0100

    Use sip API v2 (breaks default PyQt4)

--- support/rthooks/pyi_rth_qt4plugins.py
+++ support/rthooks/pyi_rth_qt4plugins.py
@@ -17,6 +17,19 @@ if 'QT_PLUGIN_PATH' in os.environ:
     os.environ['QT_PLUGIN_PATH'] = ''
     del os.environ['QT_PLUGIN_PATH']
 
+###################################################################################
+#This is in order to make pyinstaller work when using PyQt4 with api v2
+#Unfortunately it makes it stop working for applications using api v1
+#In the near future (python3) only api2 will be supported, so applications using api v1
+#should port their applications to api v2 anyway, which is, in addition, a lot 
+#more pythonic
+import sip
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+#Maybe there's a way to determine if an application is making to sip.setapi 
+#and wrap this piece of code into an if statement
+####################################################################################
+
 
 # We cannot use QT_PLUGIN_PATH here, because it would not work when
 # PyQt4 is compiled with a different CRT from Python (eg: it happens
